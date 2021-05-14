0bf616fff6894a97a9009a1e20ee677b858b6f2d
anyshamov <84168880+anyshamov@users.noreply.github.com>
2021-05-14 08:14:58 +0300

    Shamov coin

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		totalSupply = 1000000000000;
+		name = "Shamov coin";
+		decimals = 18;
+		symbol = "SHAM";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
